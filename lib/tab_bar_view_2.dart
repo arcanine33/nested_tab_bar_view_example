0bcde3caabbd0c132f4498911b9d3441fa093810
arcanine33 <leemch33@gmail.com>
2025-01-07 13:17:29 +0900

    feat: 서브탭뷰1 적용

--- lib/tab_bar_view_2.dart
+++ lib/tab_bar_view_2.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:nested_tab_bar_view/sub_tab_bar_view_1.dart';
 
 class TabBarView2 extends StatefulWidget {
   const TabBarView2({super.key});
@@ -9,11 +11,19 @@ class TabBarView2 extends StatefulWidget {
 
 class _TabBarView2State extends State<TabBarView2>  with TickerProviderStateMixin {
   late final TabController _tabController;
+  double appBarHeight = 50;
+
 
   @override
   void initState() {
     _tabController = TabController(length: 5, vsync: this);
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   @override
@@ -24,7 +34,7 @@ class _TabBarView2State extends State<TabBarView2>  with TickerProviderStateMixi
           SliverAppBar(
             toolbarHeight: 0, // between major tab space 0
             bottom: PreferredSize(
-              preferredSize: Size.fromHeight(50), // optional
+              preferredSize: Size.fromHeight(appBarHeight), // optional
               child: TabBar(
                 controller: _tabController,
                 tabs: [
@@ -42,13 +52,63 @@ class _TabBarView2State extends State<TabBarView2>  with TickerProviderStateMixi
       body: TabBarView(
         controller: _tabController,
         children: [
-          Container(child: Text('subTab one page'),),
-          Container(child: Text('subTab two page'),),
-          Container(child: Text('subTab three page'),),
-          Container(child: Text('subTab four page'),),
-          Container(child: Text('subTab five page'),),
+          SubTabBarView1(),
+          ListView.builder(
+            itemBuilder: (context, index) => ListTile(
+              title: Text('subTab two page $index'),
+            ),
+            itemCount: 100,
+          ),
+          ListView.builder(
+            itemBuilder: (context, index) => ListTile(
+              title: Text('subTab three page $index'),
+            ),
+            itemCount: 100,
+          ),
+          ListView.builder(
+            itemBuilder: (context, index) => ListTile(
+              title: Text('subTab four page $index'),
+            ),
+            itemCount: 100,
+          ),
+          ListView.builder(
+            itemBuilder: (context, index) => ListTile(
+              title: Text('subTab five page $index'),
+            ),
+            itemCount: 100,
+          ),
         ],
       ),
     );
+  }
+
+  bool _showSubTabBar(UserScrollNotification notification) {
+    if (notification.depth == 2) {
+      final currentOffset = PrimaryScrollController.of(context).offset;
+      if (notification.direction == ScrollDirection.reverse) {
+        if (currentOffset >= appBarHeight) {
+          return false;
+        }
+        PrimaryScrollController.of(context).animateTo(
+          appBarHeight,
+          duration: const Duration(milliseconds: 160),
+          curve: Curves.easeOut,
+        );
+        return false;
+      }
+
+      if (notification.direction == ScrollDirection.forward) {
+        if (currentOffset <= 0.0) {
+          return false;
+        }
+        PrimaryScrollController.of(context).animateTo(
+          0.0,
+          duration: const Duration(milliseconds: 240),
+          curve: Curves.easeOut,
+        );
+        return false;
+      }
+    }
+    return false;
   }
 }
